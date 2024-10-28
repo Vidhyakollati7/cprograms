2109c30801b6c0a175063d20a5c5011f43d956d2
Vidhyakollati7 <vidhyakollati7@gmail.com>
2024-10-28 12:33:01 +0530

    Create month.c

---
+++ month.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void main()
+{
+ int a;
+ printf("Enter the the value of a ");
+ scanf("%d",&a);
+ switch(a)
+    {
+        case1:printf("%d is Jan");
+        break;
+        case2:printf("%d is Feb");
+        break;
+        case3:printf("%de is Mar");
+        break;
+        case4:printf("%d is Apr");
+        break;
+        case5:printf("%d is May");
+        break;
+        case6:printf("%d is Jun");
+        break;
+        case7:printf("%d is Jul");
+        break;
+        case8:printf("%d is Aug");
+        break;
+        case9:printf("%d is Sep");
+        break;
+        case10:printf("%d is Oct");
+        break;
+        case11:printf("%d is Nov");
+        break;
+        case12:printf("%d is Dec");
+        break;
+        default:printf("Invalid");
+        break;
+        }
+}
