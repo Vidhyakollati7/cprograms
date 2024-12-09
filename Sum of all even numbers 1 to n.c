dc4b5afb7035d5859fa38393e369b893959fc964
Vidhyakollati7 <vidhyakollati7@gmail.com>
2024-12-09 12:15:34 +0530

    Create Sum of all even numbers 1 to n.c

---
+++ Sum of all even numbers 1 to n.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main(){
+    int i,n,sum=0;
+    printf("Enter the value of n");
+    scanf("%d",&n);
+    for(i=1;i<=n;++i)
+    {if(i%2==0)
+    {
+        sum=sum+i;}
+        
+    }
+    {
+        printf("The sum is %d",sum);
+}
+    
+}
